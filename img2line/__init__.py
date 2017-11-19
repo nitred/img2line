97a9a2ebd20ca85acd999afefad656cf925eb440
Nitish Reddy Koripalli <nitish.k.reddy@gmail.com>
2017-11-19 04:10:16 +0100

    added GaussianProcessRegression to fit line to image

--- img2line/__init__.py
+++ img2line/__init__.py
@@ -1,6 +1,9 @@
 """Lonely file consisting of the simple implementation of img2line."""
 import numpy as np
 from skimage.io import imread
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import ConstantKernel as C
+from sklearn.gaussian_process.kernels import RBF
 from sklearn.linear_model import Ridge
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
@@ -44,6 +47,27 @@ def get_line_from_image(imgpath, plot=False):
     return x, y
 
 
+def plot_line_and_model(x, y, model, label):
+    """Plot original line and model prediction."""
+    from matplotlib import pyplot as plt
+    # to keep the order
+    x_orig = np.copy(x)
+    y_orig = np.copy(y)
+
+    X_test = x_orig[:, np.newaxis]
+    # Y_test = y_orig
+
+    plt.figure()
+    plt.scatter(x_orig, y_orig,
+                color='navy',
+                s=30,
+                marker='o',
+                label="training points")
+    Y_pred = model.predict(X_test)
+    plt.plot(X_test, Y_pred, color='orange', linewidth=2, label=label)
+    plt.show()
+
+
 def fit_line_to_image(imgpath, degree=5, plot=False):
     """Function to fit a line to the image of a line.
 
@@ -71,23 +95,38 @@ def fit_line_to_image(imgpath, degree=5, plot=False):
     model.fit(X_train, Y_train)
 
     if plot is True:
-        from matplotlib import pyplot as plt
-        # to keep the order
-        x_orig = np.copy(x)
-        y_orig = np.copy(y)
+        plot_line_and_model(x, y, model, label="degree {}".format(degree))
 
-        X_test = x_orig[:, np.newaxis]
-        Y_test = y_orig
+    return x, y, model
 
-        plt.figure()
-        plt.scatter(x_orig, y_orig,
-                    color='navy',
-                    s=30,
-                    marker='o',
-                    label="training points")
-        Y_pred = model.predict(X_test)
-        plt.plot(X_test, Y_pred, color='orange', linewidth=2,
-                 label="degree %d" % degree)
-        plt.show()
+
+def fit_gpr_to_image(imgpath, plot=False):
+    """Function to fit a GaussianProcessRegressor to the image of a line.
+
+    Args:
+        imgpath (string): Full path to an image of a line.
+        plot (bool): Whether to plot the results or not.
+
+    Returns:
+        x, y, model (nd.array, nd.array, sklearn.GaussianProcessRegressor):
+        - x, y are the coordinates of the lines. The coordinates start from 0,0
+            i.e. (x[0], y[0]) = (0, 0).
+        - model can be used to predict y-coordinates from other x-coordinates by
+            calling `y_new = model.predict(x_new)`.
+    """
+    x, y = get_line_from_image(imgpath, plot=False)
+
+    # shuffle x and y
+    random_order = np.random.permutation(len(x))
+    X_train = x[random_order][:, np.newaxis]
+    Y_train = y[random_order]
+
+    # Train Model
+    kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+    model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
+    model.fit(X_train, Y_train)
+
+    if plot is True:
+        plot_line_and_model(x, y, model, label="GPR")
 
     return x, y, model
